8148297d0e6431b59d20eeaf70f4634ec22e71d8
code-dohyeon <69374356+code-dohyeon@users.noreply.github.com>
2023-10-28 10:08:45 +0000

    add scroll event

--- 2023-10-27/js/main.js
+++ 2023-10-27/js/main.js
@@ -3,7 +3,32 @@ let index = 1;
 let logo = document.querySelector("svg");
 let nav = document.querySelector("nav");
 let menus = document.querySelectorAll(".menu");
-let inner_height = window.innerHeight;
+let inner_height = [innerHeight, innerHeight*2, innerHeight*3];
+
+function addEvent() {
+    for (let i = 0; i < menus.length; i++) {
+        menus[i].addEventListener("click", () => {
+            window.scrollTo(0, inner_height[i]);
+        }, false);
+    }
+
+    window.addEventListener("scroll", e => {
+        if(window.scrollY < 929) {
+            logo.style.fill="#ffffff";
+            nav.style.color="white";
+        }
+        else { 
+            logo.style.fill="#000000";
+            nav.style.color="black";
+            }
+    }, false);
+    
+    logo.addEventListener("click", () => {
+        window.scrollTo(0, 0);
+    }, false);
+}; addEvent();
+
+
 setInterval(() => {
     console.log(index);
     if(index > 2) {
@@ -15,18 +40,4 @@ setInterval(() => {
         backgrounds[index].style="opacity: 1; z-index: 1;";
         index += 1; 
     }
-    
 }, 5000);
-
-window.addEventListener("scroll", e => {
-    if(window.scrollY < 929) {
-        logo.style.fill="#ffffff";
-        nav.style.color="white";
-    }
-    else { 
-        logo.style.fill="#000000";
-        nav.style.color="black";
-        }
-    
-});
-
