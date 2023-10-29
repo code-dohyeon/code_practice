95bbde1d796d8d29f9c1553b435913298b6afee3
code-dohyeon <69374356+code-dohyeon@users.noreply.github.com>
2023-10-29 01:09:11 +0000

    reload scroll fixed

--- 2023-10-27/js/main.js
+++ 2023-10-27/js/main.js
@@ -4,22 +4,51 @@ let nav = document.querySelector("nav");
 let menus = document.querySelectorAll(".menu");
 let header = document.querySelector("header");
 let video = document.querySelector("video");
+let html = document.querySelector("html");
 let inner_height = [0, innerHeight, innerHeight*2, innerHeight*3];
 let index = 1;
 let page = 0;
-let wheel = true;
 
-header.style.left=`${(innerWidth - 1440) / 2}px`;
-video.height=innerHeight;
+window.onbeforeunload = () => {
+    window.localStorage.setItem("scrollpos", window.scrollY);
+    console.log(window.localStorage);
+}
+
+window.onload = () => {
+    console.log(window.localStorage);
+    window.scrollTo(0, window.localStorage.getItem("scrollpos"));
+    window.localStorage.removeItem("scrollpos");
+    console.log(window.scrollY);
+}
+
+function init() {
+    header.style.left=`${(innerWidth - 1440) / 2}px`;
+    video.height=innerHeight;
+
+    setInterval(() => {
+        if(index > 2) {
+            index = 1;
+            backgrounds[1].style="opacity: 0; z-index: 0;";
+            backgrounds[2].style="opacity: 0; z-index: 0;";
+        }
+        else {
+            backgrounds[index].style="opacity: 1; z-index: 1;";
+            index += 1; 
+        }
+    }, 5000);
+}; init();
+
 
 function addEvent() {
     for (let i = 1; i < menus.length+1; i++) {
         menus[i-1].addEventListener("click", () => {
             window.scrollTo(0, inner_height[i]);
+            console.log("a");
         }, false);
     }
 
     window.addEventListener("scroll", e => {
+        e.preventDefault();
         if(window.scrollY < innerHeight*2) {
             logo.style.fill="#ffffff";
             nav.style.color="white";
@@ -28,10 +57,12 @@ function addEvent() {
             logo.style.fill="#000000";
             nav.style.color="black";
             }
-    }, false);
+        // console.log("a");
+    }, false, {passive : false});
     
     logo.addEventListener("click", () => {
         window.scrollTo(0, 0);
+        console.log("a");
     }, false);
 
     window.addEventListener("wheel", (e) => {
@@ -49,18 +80,6 @@ function addEvent() {
             console.log(`page: ${page}, height: ${inner_height[page]}`);
 
         }
+        console.log("a");
     }, { passive: false });
 }; addEvent();
-
-
-setInterval(() => {
-    if(index > 2) {
-        index = 1;
-        backgrounds[1].style="opacity: 0; z-index: 0;";
-        backgrounds[2].style="opacity: 0; z-index: 0;";
-    }
-    else {
-        backgrounds[index].style="opacity: 1; z-index: 1;";
-        index += 1; 
-    }
-}, 5000);
