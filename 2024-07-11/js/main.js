7445c346aa836561da13b432e1a36d0d38cfc09d
code-dohyeon <69374356+code-dohyeon@users.noreply.github.com>
2024-07-11 03:35:26 +0000

    2024-07-11

---
+++ 2024-07-11/js/main.js
@@ -0,0 +1,18 @@
+class Main {
+    constructor() {
+        let container = document.createElement("ul");
+            container.style = `width: 700px; height: 700px; position: relative; display: flex; align-content: center; justify-content: center; border: 1px solid black; flex-wrap: wrap;`;
+            container.className="game_container";
+
+            document.body.appendChild(container);
+
+        let container_items = document.createElement("li");
+            container_items.style=`width: calc(99%/50); height: calc(99%/50);`;
+    }
+
+
+}
+
+window.onload = () => {
+    new Main();
+}
