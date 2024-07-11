b2ce1df3f6cd24c9458ee8c11a691308979a15e9
code-dohyeon <69374356+code-dohyeon@users.noreply.github.com>
2024-07-11 03:50:32 +0000

    2024-07-11 12:50

--- 2024-07-11/js/main.js
+++ 2024-07-11/js/main.js
@@ -1,13 +1,21 @@
 class Main {
     constructor() {
-        let container = document.createElement("ul");
-            container.style = `width: 700px; height: 700px; position: relative; display: flex; align-content: center; justify-content: center; border: 1px solid black; flex-wrap: wrap;`;
+        let container = this.container = document.createElement("ul");
+            container.style = `width: 700px; height: 700px; position: relative; display: flex; align-content: center; justify-content: center; flex-wrap: wrap;`;
             container.className="game_container";
 
             document.body.appendChild(container);
 
-        let container_items = document.createElement("li");
-            container_items.style=`width: calc(99%/50); height: calc(99%/50); border: 1px solid black;`;
+            this.CreateItems();
+
+    }
+
+    CreateItems () {
+        for(let i = 0; i < 100; i++) {
+            let container_items = this.container_items = document.createElement("li");
+            container_items.style=`width: calc(100%/10); height: calc(100%/10); border: 1px solid black;`;
+            this.container.appendChild(this.container_items);
+        }
     }
 
 
