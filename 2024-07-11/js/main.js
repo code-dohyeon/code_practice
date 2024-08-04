0d9a5e41183bb20f6edaee45b39848a20fc3b889
code-dohyeon <69374356+code-dohyeon@users.noreply.github.com>
2024-08-04 14:13:14 +0000

    2024-08-04 23:12 updated.

--- 2024-07-11/js/main.js
+++ 2024-07-11/js/main.js
@@ -1,26 +1,34 @@
 class Main {
     constructor() {
-        let container = this.container = document.createElement("ul");
-            container.style = `width: 700px; height: 700px; position: relative; display: flex; align-content: center; justify-content: center; flex-wrap: wrap;`;
-            container.className="game_container";
+        let gage_bar = this.gage_bar = document.querySelector(".gage_bar");
+        let gage = this.gage;
 
-            document.body.appendChild(container);
+        let circle = this.circle = document.querySelector(".circle");
+        let visual_container = this.visual = document.querySelector(".visual_container");
 
-            this.CreateItems();
+        let radius = 0;
+        this.addEvent(radius);
 
     }
 
-    CreateItems () {
-        for(let i = 0; i < 100; i++) {
-            let container_items = this.container_items = document.createElement("li");
-            container_items.style=`width: calc(100%/10); height: calc(100%/10); border: 1px solid black;`;
-            this.container.appendChild(this.container_items);
-        }
+    addEvent(radius) {
+        window.addEventListener("scroll", e => {
+            this.gage = Math.floor(((window.scrollY + screen.height) / document.body.clientHeight) * 100);
+            // console.log(this.gage);
+
+            this.gage_bar.style.width = `${this.gage}%`;
+        });
+
+        this.visual.addEventListener("wheel", e => {
+            if(this.visual.scrollTop > 0 && this.visual.scrollTop + this.visual.clientHeight < this.visual.clientHeight) {
+                radius = e.deltaY >= 0 ? radius -= 1 : radius += 1;
+            }
+
+            console.log(this.visual.scrollTop > 0, this.visual.scrollTop + this.visual. < this.visual.clientHeight);
+        });
     }
-
-
 }
 
 window.onload = () => {
-    new Main();
+    new Main;
 }
